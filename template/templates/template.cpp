c3c2d427c8ff7918375b7569edba0715869c29e4
Huy Pham <pqhuy@amazon.com>
2022-04-02 17:34:02 +0200

    update C++ template

--- template/templates/template.cpp
+++ template/templates/template.cpp
@@ -2,18 +2,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(int test) {
-  int N, M;
-  cin >> N >> M;
-  auto a = vector<vector<int>>(N, vector<int>(M, 0));
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < M; j++) {
-      cin >> a[i][j];
-    }
-  }
+class Solver {
+  vector<vector<int>> edges;
 
-  cout << "Case #" << test << ": " << 0 << endl;
-}
+ public:
+  void solve(int test) {
+    int n, m;
+    cin >> n >> m;
+    auto a = vector<vector<int>>(n, vector<int>(m, 0));
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        cin >> a[i][j];
+      }
+    }
+
+    cout << "Case #" << test << ": " << 0 << endl;
+  }
+};
 
 int main() {
   ios::sync_with_stdio(false);
@@ -23,7 +28,7 @@ int main() {
   cin >> T;
   for (int t = 1; t <= T; t++) {
     cerr << "Case #" << t << " stderr:" << endl;
-    solve(t);
+    Solver().solve(t);
   }
   return 0;
 }
